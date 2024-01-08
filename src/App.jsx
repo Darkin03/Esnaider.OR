3203d98e7aa76b699e981a756403ee6a00e46c42
Esnaider.OR <80131310+Darkin03@users.noreply.github.com>
2024-01-07 20:19:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,23 @@ function App() {
 
       <section >
         <Routes>
-          <Route path="/" element={<AboutMe />} />
+          <Route path="/Esnaider.OR" element={<AboutMe />} />
           <Route path="/habilidades" element={<SkillsSection />} />
           <Route path="/proyectos" element={<Prcontenedor />} />
           <Route path="/contactos" element={<Contacts />} />
         </Routes>
+       
+      </section>
+
+      <Footer/>
+     
+      <Up/>
+    </>
+  );
+}
+
+export default App;
+
        
       </section>
 
