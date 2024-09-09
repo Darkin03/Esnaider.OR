a44fd6e4e00bb5225e0ba1c5b7ae79dc99819c42
Esnaider.OR <80131310+Darkin03@users.noreply.github.com>
2024-09-09 08:45:57 -0500

    Update SkillsSection.jsx

--- src/sections/SkillsSection.jsx
+++ src/sections/SkillsSection.jsx
@@ -1,6 +1,5 @@
 import webDeveloper from "../images/Data Arranging_Flatline.svg";
-import React, {useState} from 'react';
-
+import {useState} from 'react';
 import { CiStar} from "react-icons/ci";
 import { FaHtml5, FaCss3Alt, FaReact, FaBootstrap, FaNodeJs, FaPhp, FaGit } from "react-icons/fa";
 import { SiJavascript, SiTypescript, SiNextdotjs, SiMysql } from "react-icons/si";
